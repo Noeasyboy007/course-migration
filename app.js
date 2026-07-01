e12cd0028f727ffba932f072afee7900dc8e3e90
Noeasyboy007 <aritrabera69@gmail.com>
2026-07-01 14:43:04 +0530

    done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const colors = require('colors');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const courseMigrationRoutes = require('./routes/courseMigrationRoutes');
+const successStoryMigrationRoutes = require('./routes/successStroryMigrationRoutes');
 const app = express();
 
 app.use(cors());
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 9090;
 
 app.use(express.json());
 app.use('/api/course-migration', courseMigrationRoutes);
+app.use('/api/success-story-migration', successStoryMigrationRoutes);
 
 app.get('/health', (req, res) => {
     res.status(200).json({
